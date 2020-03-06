4183401f51547eba01edaa4005739c137a596cb9
Charlie <ctslone27@gmail.com>
2020-03-06 18:12:56 -0500

    adding code to connect to db

---
+++ models/book.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    authors: {
+        type: String,
+        required: true
+    },
+    description: {type: String},
+    image: {type: String},
+    link: {type: String}
+})
+
+module.exports = bookSchema;
